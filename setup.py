b0d368627be3e2c9e791e029f0fdb814297464d1
fliper0 <leviguleypc@gmail.com>
2025-01-06 10:59:00 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,23 +5,21 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'colorama',  # Ensure colorama is installed for colored output
+        'colorama',
     ],
     entry_points={
         'console_scripts': [
-            'nmap-scanner = nmap_scanner:main_menu',  # Replace with your actual entry point function and script name
+            'nmap-scanner = nmap_scanner.your_script:main_menu',
         ],
     },
     author='Your Name',
     author_email='your_email@example.com',
     description='A script to automate Nmap and SSLScan operations with a simple menu.',
-    long_description=open('README.md').read(),  # You can add a README.md file for documentation
-    long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/nmap-scanner',  # Replace with your GitHub or project URL
+    url='https://github.com/yourusername/nmap-scanner',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',  # Adjust the Python version requirement
+    python_requires='>=3.6',
 )
